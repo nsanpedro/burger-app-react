988e0b68e5bc6472fd6196ae6d6094928028490e
nicosp <nicolassanpedro@gmail.com>
2019-05-08 20:30:02 -0300

    addIngredients feature

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -4,7 +4,7 @@ import classes from './Layout.css';
 
 const layout = (props) => (
     <Aux>
-        <div>Toolbar, SideDrawer, BackdropAAAAAAAAAA</div>
+        <div>Toolbar, SideDrawer, Backdrop</div>
         <main className={classes.Content}>
             {props.children}
         </main>
