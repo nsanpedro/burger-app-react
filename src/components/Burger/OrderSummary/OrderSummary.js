b0bd05122f44324ab0cc26573666d5696ed0da83
nicosp <nicolassanpedro@gmail.com>
2019-05-30 23:39:03 -0300

    Order Summary Modal

---
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import Aux from '../../../hoc/Aux';
+
+
+const orderSummary = (props) => {
+    const ingredientSummary = Object.keys(props.ingredients)
+                                .map(igKey => {
+                                    return <li key={igKey}>
+                                            <span style={{textTransform: 'capitalize'}}>{igKey}</span>: {props.ingredients[igKey]}
+                                        </li>
+                                });
+
+    return (
+        <Aux>
+            <h3>Tu pedido!</h3>
+            <p>Tu hamburguesa tiene:</p>
+            <ul>
+                {ingredientSummary}
+            </ul> 
+            <p>Continúe hacia el Checkout</p> 
+        </Aux>
+    )
+}
+
+export default orderSummary;
