0ccea7605fbd0e63cff671b3d42714c5153afd49
nicosp <nicolassanpedro@gmail.com>
2019-08-21 23:00:19 -0300

    last

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,29 +1,35 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Aux from '../../../hoc/Aux';
 import Button from '../../UI/Button/Button';
 
+class OrderSummary extends Component {
+    componentWillUpdate(){
+        console.log('AAAA');
+    }
 
-const orderSummary = (props) => {
-    const ingredientSummary = Object.keys(props.ingredients)
+
+    render(){
+        const ingredientSummary = Object.keys(this.props.ingredients)
             .map(igKey => {
                 return <li key={igKey}>
-                        <span style={{textTransform: 'capitalize'}}>{igKey}</span>: {props.ingredients[igKey]}
+                        <span style={{textTransform: 'capitalize'}}>{igKey}</span>: {this.props.ingredients[igKey]}
                     </li>
             });
 
-    return (
-        <Aux>
-            <h3>Tu pedido!</h3>
-            <p>Tu hamburguesa tiene:</p>
-            <ul>
-                {ingredientSummary}
-            </ul> 
-            <p><strong>Precio Final: ${props.price.toFixed(2)}</strong></p>
-            <p>Continúe hacia el Checkout</p> 
-            <Button btnType="Danger" clicked={props.purchaseCancelled}>CANCELAR</Button>
-            <Button btnType="Success" clicked={props.purchaseContinued}>PAGAR</Button>
-        </Aux>
-    )
+        return(
+            <Aux>
+                <h3>Tu pedido!</h3>
+                <p>Tu hamburguesa tiene:</p>
+                <ul>
+                    {ingredientSummary}
+                </ul> 
+                <p><strong>Precio Final: ${this.props.price.toFixed(2)}</strong></p>
+                <p>Continúe hacia el Checkout</p> 
+                <Button btnType="Danger" clicked={this.props.purchaseCancelled}>CANCELAR</Button>
+                <Button btnType="Success" clicked={this.props.purchaseContinued}>PAGAR</Button>
+            </Aux>
+        );
+    }
 }
 
-export default orderSummary;
+export default OrderSummary;
